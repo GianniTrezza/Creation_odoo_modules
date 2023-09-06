e2783c085ac5bf37da4f21b9627583e99d700cf6
GianniTrezza <138105051+GianniTrezza@users.noreply.github.com>
2023-09-06 13:21:55 +0200

    Add files via upload

---
+++ new_accreditamento/__manifest__.py
@@ -0,0 +1,19 @@
+{
+    'name': 'new_accreditamento',
+    'author': 'Giovanni Trezza',
+    'category': 'Health',
+    'depends': ['base', 'mail'],
+    'website': 'www.odoo_accreditamento.tech',
+    'summary': 'Processo accreditamento (Strutture Sanitarie)', 
+    'data': [
+        'data/dati_sequenza.xml',
+        'security/ir.model.access.csv',
+        'security/sicurezza.xml',
+        'views/accreditation.xml',
+        'views/struttura_sanitaria.xml',
+        'views/tipologia_pratica.xml',
+        'views/menu.xml'      
+    ],  
+    'installable': True,
+    'application': True,
+}
