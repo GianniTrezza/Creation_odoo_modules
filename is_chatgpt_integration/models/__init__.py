dd01e96caff4bfb63f9b3638e499a78a29df530c
GianniTrezza <138105051+GianniTrezza@users.noreply.github.com>
2023-09-07 13:14:52 +0200

    Delete is_chatgpt_integration directory

--- is_chatgpt_integration/models/__init__.py
+++
@@ -1,4 +0,0 @@
-# -*- coding: utf-8 -*-
-# Copyright (c) 2020-Present InTechual Solutions. (<https://intechualsolutions.com/>)
-
-from . import chatgpt_model, mail_channel, res_config_settings, fatturazione
